53725e8a989e904fd5df7045d2ed8aa3b425d22e
weswhitney <wesleywhitney@gmail.com>
2014-11-01 22:02:15 -0600

    user crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,25 @@ class UsersController < ApplicationController
     @user.save
     redirect_to users_path
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    user_params= params.require(:user).permit(:first_name, :last_name, :email)
+    @user.update(user_params)
+    redirect_to users_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
 end
